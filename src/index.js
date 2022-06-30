23a645a8d09277655350d553b6d386334319c316
zamarrowski <sergiozamarro@hotmail.com>
2022-06-30 11:28:46 +0200

    fix module version and tests

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import reveni from './reveni'
 
 window.reveni = {
   init: (orderId, returnId, token, elementSelector, sandbox = false) =>
-    reveni.init(orderId, returnId, elementSelector, token, sandbox),
+    reveni.init(orderId, returnId, elementSelector, token, sandbox, true),
   close: elementSelector => reveni.close(elementSelector),
 }
 
 export default {
   init: (orderId, returnId, token, elementSelector, sandbox = false) =>
-    reveni.init(orderId, returnId, elementSelector, token, sandbox),
+    reveni.init(orderId, returnId, elementSelector, token, sandbox, false),
   close: elementSelector => reveni.close(elementSelector),
 }
--- src/reveni.js
+++ src/reveni.js
@@ -5,15 +5,17 @@ const close = elementSelector => {
   element.innerHTML = ''
 }
 
-const init = (orderId, returnId, elementSelector, token, sandbox) => {
-  const scriptTags = document.querySelectorAll('script')
-  const currentScript = Array.from(scriptTags).find(script => script.src.includes(process.env.SDK_OUTPUT_NAME))
-  const queryParams = getQueryParams(currentScript)
+const init = (orderId, returnId, elementSelector, token, sandbox, loadedByTag = true) => {
+  if (loadedByTag) {
+    const scriptTags = document.querySelectorAll('script')
+    const currentScript = Array.from(scriptTags).find(script => script.src.includes(process.env.SDK_OUTPUT_NAME))
+    const queryParams = getQueryParams(currentScript)
 
-  if (!orderId) orderId = queryParams.orderId
-  if (!returnId) returnId = queryParams.returnId
-  if (!elementSelector) elementSelector = queryParams.elementSelector
-  if (!token) token = queryParams.token
+    if (!orderId) orderId = queryParams.orderId
+    if (!returnId) returnId = queryParams.returnId
+    if (!elementSelector) elementSelector = queryParams.elementSelector
+    if (!token) token = queryParams.token
+  }
 
   validateInitParams(orderId, returnId, elementSelector)
   const url = generateUrl(orderId, returnId, token, sandbox)
